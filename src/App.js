0e168c796303bb34046f004e96f9d628f2c1bd67
Dev4281 <divyansh.sharma0210@gmail.com>
2022-11-12 16:31:02 +0530

    TodoDelete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import NavbarComp from './myComponents/NavbarComp';
+import Todos from './myComponents/Todos';
+import Footer from './myComponents/Footer';
+
 
 function App() {
+  const onDelete =(todo)=>
+  {
+    console.log("I am onDelete", todo);
+  }
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get this job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to go to the market to get this job done"
+    },
+    {
+      sno: 3,
+      title: "Go to the ghat",
+      desc: "You need to go to the market to get this job done"
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+   <NavbarComp title="MyTodosList" searchBar={false}/>
+   <Todos todos={todos} onDelete={onDelete}/>
+   <Footer/>
+
+   
+  </div>
   );
 }
 
